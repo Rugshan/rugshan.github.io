66930cf73d02ba977e60595e264c7214584d66a7
Rugshan <56658716+Rugshan@users.noreply.github.com>
2025-08-31 13:07:46 -0400

    updated sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://rugshan.github.io',
+  site: 'https://rugshan.dev',
   integrations: [
     tailwind(),
-    sitemap()
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
   ],
   build: {
     assets: 'assets'
